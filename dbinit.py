c04bf80f1c3d626605c1b685c5bb9f44e6d6eeac
unknown <beraatbuz@gmail.com>
2019-11-04 22:10:26 +0300

    database url added

--- dbinit.py
+++ dbinit.py
@@ -154,7 +154,7 @@ def initialize(url):
 
 
 if __name__ == "__main__":
-    url = os.getenv("DATABASE_URL")
+    url = "postgres://ydsnhphm:afmHtP2dhNoOfJQA7f_aX7YaaF9GMKWP@salt.db.elephantsql.com:5432/ydsnhphm"
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
